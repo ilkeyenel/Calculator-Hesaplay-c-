450fd426b38ef714e201b751f0b9e62c54ec18d1
ilkeyenel <ilketopac@gmail.com>
2023-11-07 10:59:33 +0300

    dosya eklendi

--- main.js
+++ main.js
@@ -12,12 +12,20 @@ const formBtn = document.querySelector(".ekle-btn");
 //console.log(formBtn)
 
 const list = document.querySelector(".list");
-const totalInfo=document.querySelector("#total-info")
+const totalInfo = document.querySelector("#total-info");
 //console.log(totalInfo)
-const nameInput=document.getElementById("name-input")
+const nameInput = document.getElementById("name-input");
+
+const statusCheck = document.getElementById("status-input");
+
+const selectFilter = document.getElementById("filter-select");
+
+const harcamaKartlari = document.querySelector(".harcamaKArtlari");
 
 formBtn.addEventListener("click", addExpense);
-list.addEventListener("click",handleClick)
+list.addEventListener("click", handleClick);
+
+selectFilter.addEventListener("change", handleFilter);
 
 const userName = localStorage.getItem("name");
 
@@ -28,34 +36,26 @@ nameInput.addEventListener("change", (e) => {
   localStorage.setItem("name", e.target.value);
 });
 
+let toplam = 0;
 
-
-
-
-
-let toplam=0
-
-function updateToplam(fiyatBilgisi){
-toplam +=Number(fiyatBilgisi);
-totalInfo.innerText=toplam
-
+function updateToplam(fiyatBilgisi) {
+  toplam += Number(fiyatBilgisi);
+  totalInfo.innerText = toplam;
 }
-
-
 
 function addExpense(e) {
   e.preventDefault();
   console.log("addExpense");
 
-
-if(!harcamaInput.value || !fiyatInput.value){
-  alert("tüm boş alanları doldurun")
-  
-}else {
-
-  const harcamaDiv = document.createElement("div");
-  harcamaDiv.classList.add("expense");
-  harcamaDiv.innerHTML = `<h2>${harcamaInput.value}</h2>
+  if (!harcamaInput.value || !fiyatInput.value) {
+    alert("tüm boş alanları doldurun");
+  } else {
+    const harcamaDiv = document.createElement("div");
+    harcamaDiv.classList.add("expense");
+    if (statusCheck.checked) {
+      harcamaDiv.classList.add("payed");
+    }
+    harcamaDiv.innerHTML = `<h2>${harcamaInput.value}</h2>
     <h2 id='value'>${fiyatInput.value}</h2>
         <div class="buttons">
              <img id='payment' src="./img/pay.png" alt="">
@@ -63,26 +63,55 @@ if(!harcamaInput.value || !fiyatInput.value){
         </div>
  `;
 
-  list.appendChild(harcamaDiv);
-  //console.log(harcamaDiv);
-  updateToplam(fiyatInput.value)
-  
+    list.appendChild(harcamaDiv);
+    //console.log(harcamaDiv);
+    updateToplam(fiyatInput.value);
+  }
+
+  harcamaInput.value = "";
+  fiyatInput.value = "";
 }
 
-  harcamaInput.value='';
-  fiyatInput.value='';
+function handleClick(e) {
+  console.log(e.target);
+  let tiklanilanEleman = e.target;
+  if (tiklanilanEleman.id === "remove") {
+    //console.log(tiklanilanEleman.parentElement.parentElement)
+    const kapsayiciElement = tiklanilanEleman.parentElement.parentElement;
+    const deletedPrice = kapsayiciElement.querySelector("#value").innerText;
+    updateToplam(-Number(deletedPrice));
+
+    kapsayiciElement.remove();
+  }
 }
 
-function handleClick(e){
-  console.log(e.target)
-  let tiklanilanEleman=e.target
-if(tiklanilanEleman.id==="remove"){
-  //console.log(tiklanilanEleman.parentElement.parentElement)
-  const kapsayiciElement = tiklanilanEleman.parentElement.parentElement;
-  const deletedPrice=kapsayiciElement.querySelector("#value").innerText
-  updateToplam(-Number(deletedPrice))
-  
-  kapsayiciElement.remove()
-}
+function handleFilter(e) {
+  //console.log("filtre fonks")
+  const items = list.childNodes;
+  //console.log("items")
+  const filterValue = e.target.value;
 
+  harcamaKartlari.forEach((harcamaKarti) => {
+    switch (filterValue) {
+      case "all":
+        harcamaKarti.style.display = "flex";
+        break;
+
+      case "payed":
+        if (!harcamaKarti.classList.contains("payed")) {
+          harcamaKarti.style.display = "none";
+        } else {
+          harcamaKarti.style.display = "flex";
+        }
+        break;
+
+      case "not-payed":
+        if (harcamaKarti.classList.contains("payed")) {
+          harcamaKarti.style.display = "none";
+        } else {
+          harcamaKarti.style.display = "flex";
+        }
+        break;
+    }
+  });
 }
